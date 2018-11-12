9531dcd1972299922d404f7e8bc002e49d2fa61b
DeWil <dewilmail@protonmail.com>
2018-11-12 18:39:48 +0100

    Updated the main page
    - First try on live exchanges
    - Background image
    - Bottom columns

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,1 @@
 // Replace current Site Content with Start Site
-$(document).ready(function(){
-	$("#current-site").load("html/start-start.html");
-});
--- js/pages.js
+++ js/pages.js
@@ -2,35 +2,20 @@
     Erstelle den Graphen für den Banner
 */
 function loadIndex() {
-  var ctx = document.getElementById('index-chart').getContext('2d');
-  var chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: 'line',
+}
 
-      // The data for our dataset
-      data: {
-          labels: ["January", "February", "March", "April", "May", "June", "July"],
-          datasets: [{
-              label: "Bitcoin Kurs",
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45],
-          }]
-      },
+// Lade aktuelle Wechselkurse
+function getExchanges() {
+  var a = httpGet('https://x-crypto.com/api/ticker/btc/eur');
+  console.log(a);
+  //document.getElementById('btc-kurs').innerHTML =
+}
 
-      // Configuration options go here
-      options: {
-        legend: {
-            display: false,
-        },
-        scales:{
-            xAxes: [{
-                display: false //this will remove all the x-axis grid lines
-            }],
-            yAxes: [{
-                display: false //this will remove all the x-axis grid lines
-            }]
-        }
-      }
-  });
+// Hilfsfunktion zum Erhalten von Webdaten
+function httpGet(url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
