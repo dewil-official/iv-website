49d079599452b74df022920478c234f52942a374
DeWil <dewilmail@protonmail.com>
2018-11-16 13:03:19 +0100

    Fixed bug with HTTP Request
    - Thanx to jQuery

--- js/mainpage.js
+++ js/mainpage.js
@@ -5,7 +5,6 @@
 const btc_url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR";
 const eth_url = "https://api.coinmarketcap.com/v2/ticker/1/?convert=EUR";
 
-
 function loaded() {
   // getExchanges();
   getExchanges();
@@ -13,16 +12,9 @@ function loaded() {
 
 // Http Request an CryptoCompare für die Daten.
 function getExchanges() {
-  const Http = new XMLHttpRequest();
 
-  Http.open("GET", btc_url);
-  Http.send();
-  Http.onreadystatechange=(e)=>{
-    try {
-      var obj = JSON.parse(Http.responseText); // Convert response string to object
-    } catch (err) { console.log(err); }
+  $.get(btc_url, function(responseText) {
+    document.getElementById("btc-kurs").innerHTML = responseText["EUR"] + "€";
+  });
 
-    console.log(obj["EUR"]); // Testing
-    document.getElementById("btc-kurs").innerHTML = obj["EUR"] + "€"; // Display it on page
-  }
 }
