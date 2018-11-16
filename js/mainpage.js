5123042ced4846d4b5c9140efea802d96c749dbf
DeWil <dewilmail@protonmail.com>
2018-11-16 12:33:42 +0100

    Updated Crypto Exchanges
    - New (working) API for exchange data

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,20 +1,28 @@
 /*
     Erstelle den Graphen für den Banner
 */
-async function loaded() {
+// API: https://min-api.cryptocompare.com/
+const btc_url = "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR";
+const eth_url = "https://api.coinmarketcap.com/v2/ticker/1/?convert=EUR";
+
+
+function loaded() {
+  // getExchanges();
   getExchanges();
 }
 
-// Lade aktuelle Wechselkurse
-async function getExchanges() {
-  console.log("Getting currency exchanges...");
-  // API: https://github.com/ccxt/ccxt
-  console.log (ccxt.exchanges)
-  // Binance Market
-  const binance = new ccxt.binance({
-    'enableRateLimit': true,
-  });
-  console.log(await binance.loadMarkets());
-  //console.log(binance.markets['BTC/USD']);
+// Http Request an CryptoCompare für die Daten.
+function getExchanges() {
+  const Http = new XMLHttpRequest();
 
+  Http.open("GET", btc_url);
+  Http.send();
+  Http.onreadystatechange=(e)=>{
+    try {
+      var obj = JSON.parse(Http.responseText); // Convert response string to object
+    } catch (err) { console.log(err); }
+
+    console.log(obj["EUR"]); // Testing
+    document.getElementById("btc-kurs").innerHTML = obj["EUR"] + "€"; // Display it on page
+  }
 }
