eda4dff4a209916873c94c6b042f93d1cc7c4569
DeWil <dewilmail@protonmail.com>
2018-12-02 21:27:47 +0100

    Updated Newsfeed
    - The Newsfeed has now a proper responsive design

--- js/newspage.js
+++ js/newspage.js
@@ -4,9 +4,28 @@
 */
 const rss_url = "https://cryptocurrencynews.com/feed/";
 
-const outerHtml = '<ul class="news">{entries}</ul>';
+const outerHtml = '<div class="news">{entries}</div>';
 
-const innerHtml = '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>';
+const innerHtml = '<article class="message"> <div class="message-body columns is-mobile"> <div class="column is-one-fifth"> <figure class="image"> <img src={teaserImage} </figure> </div><div class="column"> <strong>{title}</strong><br><p>{shortBody}</p><br><a href="{url}">Mehr...</a></div></div></article>';
+
+/* Raw inner HTML:
+
+<article class="message">
+  <div class="message-body columns is-mobile">
+    <div class="column is-one-fifth">
+      <figure class="image">
+        <img src={teaserImage}
+      </figure>
+    </div>
+    <div class="column">
+      <strong>{title}</strong><br>
+      <p>{shortBody}</p><br>
+      <a href="{url}">Mehr...</a>
+    </div>
+  </div>
+</article>
+
+*/
 
 // Wie der Feed dargestellt werden soll.
 const options = {
