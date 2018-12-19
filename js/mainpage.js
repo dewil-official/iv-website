d919ec54ab5c3db4d7d08a718bebb3b06cef23e1
DeWil <dewilmail@protonmail.com>
2018-12-19 21:15:07 +0100

    Made Navbars Responsive

--- js/mainpage.js
+++ js/mainpage.js
@@ -7,6 +7,16 @@ const api_url = "https://min-api.cryptocompare.com/data/";
 function loaded() {
   // getExchanges();
   getExchanges();
+  fallbackExchanges();
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
 }
 
 // Http Request an CryptoCompare für die Daten.
@@ -49,5 +59,23 @@ function setCurrency(name) {
     // Bei Erhalt der Daten, aktualisiere die Seite.
     document.getElementById(name + "-kurs").innerHTML = responseText["EUR"] + "€";
   });
+
+}
+
+function fallbackExchanges() {
+  // Wenn nach 6 Sekunden die Kurse nicht geladen werden konnten, setze alternative Werte vom 19.12.2018
+  setTimeout(function () {
+    if (document.getElementById("btc-kurs").innerHTML == "Loading...") {
+      document.getElementById("btc-kurs").innerHTML = "3343.74€";
+      document.getElementById("eth-kurs").innerHTML = "91.47€";
+      document.getElementById("ltc-kurs").innerHTML = "26.46€";
+      document.getElementById("xrp-kurs").innerHTML = "0.3242€";
+      document.getElementById("bch-kurs").innerHTML = "122.03€";
+      document.getElementById("xmr-kurs").innerHTML = "42.04€";
+      document.getElementById("dash-kurs").innerHTML = "67.86€";
+      document.getElementById("doge-kurs").innerHTML = "0.00241€";
+    }
+  }, 6000);
+
 
 }
--- js/newspage.js
+++ js/newspage.js
@@ -44,4 +44,12 @@ const options = {
 
 function loaded() {
   $("#current-site").rss(rss_url, options);
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
 }
